4fca26487fd001a78d512332c54f62b45d6b74fe
Visibus <visibus@mail.ru>
2018-09-21 13:54:45 +0300

    Учебный проект: в движении

--- js/dialog.js
+++ js/dialog.js
@@ -71,4 +71,57 @@
     userDialogFireball.querySelector('input').value = rand;
     userDialogFireball.style.background = rand;
   });
+
+  // var setupDialogElement = document.querySelector('.setup');
+  var userDialogHandler = userDialog.querySelector('.upload');
+
+  userDialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evtDr) {
+          evtDr.preventDefault();
+          userDialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        userDialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
 })();
